ea61120806d39ff6a38d7937b45ad82b20e380f4
amathur0216 <73080251+amathur0216@users.noreply.github.com>
2021-01-04 19:56:50 -0500

    Add files via upload

---
+++ block.js
@@ -0,0 +1,20 @@
+class Block extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("sprites/wood1.png");
+      this.Visiblity = 255;
+    }
+display(){
+  push();
+  if(this.body.speed < 3 ){
+    this.visiblity = 255;
+    }
+   else {
+      World.remove(world, this.body);
+      this.Visiblity = this.Visiblity - 5;
+      tint(255, this.Visiblity);
+    }
+    pop();
+}
+  
+  };
